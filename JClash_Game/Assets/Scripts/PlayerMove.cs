7cf4b13d4e1a9501817e0c49d513356d575d043a
Rimon Akhter <rimona.bevan@gmail.com>
2022-06-06 14:28:11 +0600

    Recruitment and Fight mechanics

--- JClash_Game/Assets/Scripts/PlayerMove.cs
+++ JClash_Game/Assets/Scripts/PlayerMove.cs
@@ -7,16 +7,21 @@ public class PlayerMove : MonoBehaviour
 {
 
     public Animator playerAController;
-    public float speed = 200f;
-    public float strafeSpeed = 150f;
+    public float speed = 5f;
+    public float strafeSpeed = 4f;
     private float horizontalMove;
     private bool moveByKey;
+    public bool fight;
+    public bool reached;
+    public Transform target;
+    private Rigidbody rb;
     private static readonly int Run = Animator.StringToHash("Run");
-    public Rigidbody playerRB;
+    private static readonly int Die = Animator.StringToHash("Die");
     // Start is called before the first frame update
     void Start()
     {
-        //playerRB = transform.GetChild(1).GetComponent<Rigidbody>();
+        fight = false;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,40 +29,55 @@ public class PlayerMove : MonoBehaviour
     {
         
         horizontalMove = Input.GetAxis("Horizontal");
+        Vector3 targetRot = new Vector3(horizontalMove, 0f, 1.0f);
+        Quaternion rotateToQ = Quaternion.LookRotation(targetRot);
+
         moveByKey = horizontalMove != 0;
-        
-        
-        if (moveByKey)
-        {
-            speed = 200f;
-            //transform.localPosition += new Vector3(horizontalMove * Time.deltaTime * strafeSpeed, 0f, speed * Time.deltaTime);
-            playerAController.SetBool(Run, true);
-        }
-        else
-        {
-            speed = 0f;
-            playerAController.SetBool(Run, false);
-        }
 
-        if (playerRB.velocity.magnitude > 0.5f)
+
+
+        if (fight)
         {
-            playerRB.rotation = Quaternion.Slerp(playerRB.rotation, Quaternion.LookRotation(playerRB.velocity, Vector3.up), Time.deltaTime * speed);
+            transform.LookAt(target);
+            if (!reached)
+            {
+                playerAController.SetBool(Run, true);
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, target.position, Time.deltaTime * speed * 2f);
+            }
         }
         else
         {
-            playerRB.rotation = Quaternion.Slerp(playerRB.rotation, Quaternion.identity, Time.deltaTime * speed);
+            if (moveByKey)
+            {
+                speed = 5f;
+                transform.localPosition += new Vector3(horizontalMove * Time.deltaTime * strafeSpeed, 0f, speed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotateToQ, speed * Time.deltaTime);
+                playerAController.SetBool(Run, true);
+            }
+            else
+            {
+                speed = 0f;
+                playerAController.SetBool(Run, false);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, speed * Time.deltaTime);
+            }
         }
+        
+
         
     }
-
-    private void FixedUpdate()
+    
+    private void OnCollisionEnter(Collision other)
     {
-        if (moveByKey)
+        if (other.collider.CompareTag($"enemy"))
         {
-            playerRB.velocity = new Vector3(horizontalMove * Time.deltaTime * strafeSpeed, 0f, speed * Time.deltaTime);
+            reached = true;
+            playerAController.SetBool(Die, true);
+            //playerAController.SetBool(Run, false);
+            Destroy(rb);
+            GetComponent<CapsuleCollider>().enabled = false;
         }
-        else playerRB.velocity = Vector3.zero;
     }
+
 
 
 }
